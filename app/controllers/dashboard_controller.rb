76bc1993e1ec4a4bba235cccbf67ddd353e961f9
Joe Chin <mail@joech.in>
2012-08-19 22:58:50 -0700

    first commit

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,67 @@
+class DashboardController < ApplicationController
+
+  def index 
+    require 'net/http'
+    require 'rubygems'
+    require 'json'
+      
+    # settings
+    host             = 'evidevjs1.evisions.com'
+    port             = 443
+    username         = 'ca_user'
+    password         = 'ca_user'
+    datablock        = 281
+    hash             = '5a2fb8e0701ae1c44c6966698c894278'
+    sessionParam     = "sessionid"
+    setupSessionURI  = "/mw/Session.Setup?Version=4.0&JSONData={\"Mapplets\":[{\"Guid\":\"B052A35E-DC3B-4283-B732-7BEE3B095C5E\",\"Version\":\"4.0\"}]}"
+    authenticateURI  = "/mw/Session.Authenticate?username=#{username}&password=#{password}&#{sessionParam}="
+    securityTokenURI = "/mw/Session.SecurityToken.Create?#{sessionParam}="
+    uri              = URI("https://evidevjs1.evisions.com") 
+     
+    def getData(siteURI, httpObject)
+      request  = Net::HTTP::Get.new siteURI
+      response = httpObject.request request  
+      return JSON.parse response.body
+    end
+     
+    Net::HTTP.start(uri.host, uri.port,
+      use_ssl: uri.scheme == 'https',
+      verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+      
+      # set up the session and retrieve the session id
+      data = getData(setupSessionURI, http)
+      if not data['valid']
+        @flash = 'Could not setup session'
+        exit
+      end
+     
+      # Get the session and add it to the params of future requests
+      session = data['data']['SessionId']
+      if session.nil?
+        @flash = 'You do not have the latest MAPS service'
+        exit
+      end
+     
+      authenticateURI  += session
+      securityTokenURI += session
+     
+      # authenticate the session
+      data = getData(authenticateURI, http)
+      if not data['valid']
+        @flash = 'Could not authenticate session'
+        exit
+      end
+     
+      # create the security token
+      data = getData(securityTokenURI, http)   
+      if not data['valid']
+        @flash = 'Could not get token'
+        exit
+      end
+     
+      token = data['data']['Token']
+      @url = "https://#{host}:#{port}/ArgosWeb/build/#!DataBlock=#{datablock}&Username=#{username}&Token=#{token}&Hash=#{hash}"
+    end    
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmbeddedArgosWeb::Application.config.secret_token = 'df31ab59e684c899788482ef8548f40e7a06cb987b484bad74fa4106bde8447c75b316c7198a9b5fd0aaea7792db404709beff82fee39ff1734ef9a86c220e00'
