bc947836367dc3adec5593197a91a5c758434252
Christopher Nguyen <christoph.nguyen@gmail.com>
2013-05-03 01:25:39 -0700

    updated yml files for rails 3.2.13 and fixed issues with url

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@ class DashboardController < ApplicationController
 
   def index 
     endpoint = 'https://evidevjs1.evisions.com/'
-    port  = 443
     client_username = "ca_user"
     client_password = "ca_user"
     hash  = '5a2fb8e0701ae1c44c6966698c894278'
@@ -17,7 +16,7 @@ class DashboardController < ApplicationController
     response = client.session_setup
 
     @flash = 'Could not setup session' and exit unless response.valid
-    @flash = 'Could not find session id' and exit unless reponse.data.SessionId
+    @flash = 'Could not find session id' and exit unless response.data.SessionId
 
     session = response.data.SessionId
 
@@ -31,7 +30,7 @@ class DashboardController < ApplicationController
 
     token = response.data.Token
 
-    @url = "#{endpoint}:#{port}/Argos/AWV/#!DataBlock=#{datablock}&Username=#{username}&Token=#{token}&Hash=#{hash}"     
+    @url = "#{endpoint}Argos/AWV/#!DataBlock=#{datablock}&Username=#{client_username}&Token=#{token}&Hash=#{hash}"     
   end
 
 end
