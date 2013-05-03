7e1835a54791f64f906065f2ba6fd2fbc81f25bc
Christopher Nguyen <christoph.nguyen@gmail.com>
2013-05-03 01:08:26 -0700

    Changed file to use eviapi gem

--- Gemfile
+++ Gemfile
@@ -1,10 +1,13 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.3'
+gem 'rails', '3.2.13'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 gem 'pg'
+gem 'eviapi', :git => "https://github.com/lestopher/eviapi.git"
+gem 'thin'
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,67 +1,37 @@
 class DashboardController < ApplicationController
 
   def index 
-    require 'net/http'
-    require 'rubygems'
-    require 'json'
-      
-    # settings
-    host             = 'evidevjs1.evisions.com'
-    port             = 443
-    username         = 'ca_user'
-    password         = 'ca_user'
-    datablock        = 297
-    hash             = '5a2fb8e0701ae1c44c6966698c894278'
-    sessionParam     = "sessionid"
-    setupSessionURI  = "/mw/Session.Setup?Version=4.0&JSONData={\"Mapplets\":[{\"Guid\":\"B052A35E-DC3B-4283-B732-7BEE3B095C5E\",\"Version\":\"4.0\"}]}"
-    authenticateURI  = "/mw/Session.Authenticate?username=#{username}&password=#{password}&#{sessionParam}="
-    securityTokenURI = "/mw/Session.SecurityToken.Create?#{sessionParam}="
-    uri              = URI("https://evidevjs1.evisions.com") 
-     
-    def getData(siteURI, httpObject)
-      request  = Net::HTTP::Get.new siteURI
-      response = httpObject.request request  
-      return JSON.parse response.body
-    end
-     
-    Net::HTTP.start(uri.host, uri.port,
-      use_ssl: uri.scheme == 'https',
-      verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
-      
-      # set up the session and retrieve the session id
-      data = getData(setupSessionURI, http)
-      if not data['valid']
-        @flash = 'Could not setup session'
-        exit
-      end
-     
-      # Get the session and add it to the params of future requests
-      session = data['data']['SessionId']
-      if session.nil?
-        @flash = 'You do not have the latest MAPS service'
-        exit
-      end
-     
-      authenticateURI  += session
-      securityTokenURI += session
-     
-      # authenticate the session
-      data = getData(authenticateURI, http)
-      if not data['valid']
-        @flash = 'Could not authenticate session'
-        exit
-      end
-     
-      # create the security token
-      data = getData(securityTokenURI, http)   
-      if not data['valid']
-        @flash = 'Could not get token'
-        exit
-      end
-     
-      token = data['data']['Token']
-      @url = "https://#{host}:#{port}/ArgosWeb/#!DataBlock=#{datablock}&Username=#{username}&Token=#{token}&Hash=#{hash}"
-    end    
+    endpoint = 'https://evidevjs1.evisions.com/'
+    port  = 443
+    client_username = "ca_user"
+    client_password = "ca_user"
+    hash  = '5a2fb8e0701ae1c44c6966698c894278'
+    datablock  = 297
+
+    client = Eviapi.client
+
+    client.client_username = client_username
+    client.client_password = client_password
+    client.endpoint           = endpoint
+
+    response = client.session_setup
+
+    @flash = 'Could not setup session' and exit unless response.valid
+    @flash = 'Could not find session id' and exit unless reponse.data.SessionId
+
+    session = response.data.SessionId
+
+    response = client.session_authenticate
+
+    @flash = 'Could not authenticate user' and exit unless response.valid
+
+    response = client.session_securitytoken_create
+
+    @flash = 'Could not get token' and exit unless response.valid
+
+    token = response.data.Token
+
+    @url = "#{endpoint}:#{port}/Argos/AWV/#!DataBlock=#{datablock}&Username=#{username}&Token=#{token}&Hash=#{hash}"     
   end
 
 end
